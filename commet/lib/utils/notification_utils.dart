2cb3a3aeb140fabdc7170f0de48d8abc5428f062
TheAnnoyingFruit <111474697+TheAnnoyingFruit@users.noreply.github.com>
2026-05-25 17:45:59 -0400

    Revert "Add tray"

--- commet/lib/utils/notification_utils.dart
+++ commet/lib/utils/notification_utils.dart
@@ -2,14 +2,6 @@ import 'package:commet/main.dart';
 
 class NotificationUtils {
   static (int, int) getNotificationCounts() {
-    return _getNotificationCounts(includeSingleRooms: false);
-  }
-
-  static (int, int) getTrayNotificationCounts() {
-    return _getNotificationCounts(includeSingleRooms: true);
-  }
-
-  static (int, int) _getNotificationCounts({required bool includeSingleRooms}) {
     var highlightedNotificationCount = 0;
     var notificationCount = 0;
 
@@ -21,17 +13,8 @@ class NotificationUtils {
       notificationCount += i.displayNotificationCount;
     }
 
-    if (includeSingleRooms) {
-      // Include rooms that are not part of any space and are not direct messages.
-      for (var room in clientManager!.singleRooms()) {
-        highlightedNotificationCount +=
-            room.displayHighlightedNotificationCount;
-        notificationCount += room.displayNotificationCount;
-      }
-    }
-
     for (var dm in clientManager!.directMessages.highlightedRoomsList) {
-      highlightedNotificationCount += dm.displayHighlightedNotificationCount;
+      highlightedNotificationCount += dm.displayNotificationCount;
       notificationCount += dm.displayNotificationCount;
     }
 
